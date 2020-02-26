8e558056f0e6746c06355403f6ba2d5290cfa98c
QuaintShanty <kristopher.ali.dev@gmail.com>
2020-02-26 09:38:46 -0500

    Converted to the custom routes system.

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 import './App.scss';
 
+import { Switch } from 'react-router-dom';
+import Routes from 'components/routes/Routes';
+import RootRoutes from 'components/routes/Root.Route';
+
 export default function App() {
-  return <>{'owiki!'}</>;
+  return (
+    <Switch>
+      <Routes routes={RootRoutes} />
+    </Switch>
+  );
 }
