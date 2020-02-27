019ad16e736de70a6686f68e3656a776bedee400
QuaintShanty <kristopher.ali.dev@gmail.com>
2020-02-27 04:27:14 -0500

    Changed the home page to utilize the profile state minimally.

--- react/src/components/routes/pages/Home/Home.tsx
+++ react/src/components/routes/pages/Home/Home.tsx
@@ -2,6 +2,38 @@ import React from 'react';
 
 import './Home.scss';
 
+import { useUserInfoValue } from 'components/contexts/data/UserInfo';
+import { db } from 'components/firebase/firebase.main';
+
+import { Link } from 'react-router-dom';
+
 export default function Home() {
-  return <>{'owiki!'}</>;
+  const [userInfo] = useUserInfoValue();
+
+  let response;
+  const { user } = userInfo;
+  const { profile } = userInfo;
+
+  if (user !== null && profile !== null) {
+    response = <div>{`Hello, ${profile.username}!`}</div>;
+  } else {
+    if (user === null) {
+      response = (
+        <div>
+          {'Feel free to'}
+          <Link to="/register">{'sign up'}</Link>
+          {' or '}
+          <Link to="/login">{'login'}</Link>
+          {'.'}
+        </div>
+      );
+    }
+  }
+
+  return (
+    <>
+      <div>{'owiki!'}</div>
+      {response}
+    </>
+  );
 }
