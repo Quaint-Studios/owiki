d69ec6b710e057848d5f615d18cf30bc1d896268
QuaintShanty <kristopher.ali.dev@gmail.com>
2020-02-27 04:22:13 -0500

    Added the login and register routes.

--- react/src/components/routes/Root.Route.tsx
+++ react/src/components/routes/Root.Route.tsx
@@ -1,6 +1,8 @@
 import { IRoute } from './Routes';
 
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 import Error404 from './pages/Error404/Error404';
 
 const RootRoutes: IRoute[] = [
@@ -8,6 +10,16 @@ const RootRoutes: IRoute[] = [
     name: 'Home',
     path: '/',
     component: Home
+  },
+  {
+    name: 'Login',
+    path: '/login',
+    component: Login
+  },
+  {
+    name: 'Register',
+    path: '/register',
+    component: Register
   },
   {
     name: 'Error 404',
