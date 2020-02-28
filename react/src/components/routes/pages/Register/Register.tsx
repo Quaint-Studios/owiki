562353d45f6602ae9af6c259882f30aac48f82ad
QuaintShanty <kristopher.ali.dev@gmail.com>
2020-02-28 00:16:51 -0500

    Fixed multiple cases warning.

--- react/src/components/routes/pages/Register/Register.tsx
+++ react/src/components/routes/pages/Register/Register.tsx
@@ -44,7 +44,6 @@ export default function Register() {
 
     switch (fieldType) {
       case 'email':
-      case 'all':
         /*eslint no-control-regex: 0 */
         const rgx = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
@@ -56,7 +55,6 @@ export default function Register() {
         break;
 
       case 'username':
-      case 'all':
         if (username.length < 3) {
           prevErrors.username = 'Username too short.';
           break;
@@ -65,7 +63,6 @@ export default function Register() {
         break;
 
       case 'password':
-      case 'all':
         if (password.length < 8) {
           prevErrors.password = 'Password too short.';
           break;
